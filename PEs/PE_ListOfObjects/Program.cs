dc2f613f8507744fbda1da7b2358a1b194a8e072
esmvcs <esmvcs@rit.edu>
2025-01-24 09:16:16 -0500

    F 1/24 in class demo of file IO

--- PEs/PE_ListOfObjects/Program.cs
+++ PEs/PE_ListOfObjects/Program.cs
@@ -6,6 +6,7 @@ namespace PE_ListOfObjects
     {
         static void Main(string[] args)
         {
+
             Student test = new Student("Shiro", "Cooking", 4);
             Console.WriteLine(test);
             TestRoster();
@@ -13,11 +14,12 @@ namespace PE_ListOfObjects
 
         private static void TestRoster()
         {
-            Roster roster = new Roster("Sample");
+            Roster roster = new Roster("SampleRoster");
             roster.AddStudent(new Student("Shiro", "Cooking", 4));
             roster.AddStudent(new Student("Lacy", "Baking", 3));
             roster.AddStudent(new Student("Pax", "Barking", 2));
             roster.AddStudent(new Student("Aiden", "Eating", 1));
+            roster.Save();
         }
     }
 }
--- PEs/PE_ListOfObjects/Roster.cs
+++ PEs/PE_ListOfObjects/Roster.cs
@@ -1,14 +1,72 @@
-﻿namespace PE_ListOfObjects
+﻿using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace PE_ListOfObjects
 {
     internal class Roster
     {
         private List<Student> students;
         public string Name { private set; get; }
+        private string filename;
 
-        public Roster(string name)
+        public Roster(string name) // read here
         {
             this.Name = name;
             students = new List<Student>();
+
+            // Uses object’s roster name as a potential filename
+            // (e.g. “All Students.txt” with the correct relative path)
+            // and writes all students to the file (in a format of your choice).
+            filename = "../../../" + Name + ".txt";
+
+            // Check if it exists using the .NET File class.
+
+            // IF the file exists, use it to populate the roster List(by reading it
+            // based on the file format you used in Save)
+
+            // If it doesn’t exist, do nothing extra. The constructor finishes with an
+            // empty students List just as it did before.
+
+        }
+
+        public void Save() // we'll write here
+        {
+            // Create the variable here since we need it
+            // after the try
+            StreamWriter writer = null;
+
+            try
+            {
+                // When we open for writing, create
+                // the file if it doesn't exist yet
+                writer = new StreamWriter(filename);
+
+                // Write some data
+                /*
+                writer.Write("Hello ");
+                writer.WriteLine("there!");
+                writer.WriteLine("This works!");
+                */
+                foreach (Student s in students)
+                {
+                    writer.WriteLine($"{s.Name},{s.Major},{s.Year}");
+                    // Name,Major,Year
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error writing: " + e.Message);
+            }
+
+            // for testing only
+            // SmartConsole.Prompt("Press enter to close & save.");
+
+            // Close it as long as it was actually opened
+            if (writer != null)
+            {
+                writer.Close();
+            }
         }
 
         public Student SearchByName(string name)
