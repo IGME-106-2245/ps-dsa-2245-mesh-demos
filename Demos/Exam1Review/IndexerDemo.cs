8c83b544db16de00f38441db8f506a253deca516
esmvcs <esmvcs@rit.edu>
2025-02-12 09:20:24 -0500

    W 2/12 - misc exam review code

---
+++ Demos/Exam1Review/IndexerDemo.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam1Review
+{
+    internal class IndexerDemo
+    {
+        private char[] grades;
+
+        public char this[int assignmentNum]
+        {
+            get
+            {
+                return grades[assignmentNum];
+            }
+
+            set
+            {
+                grades[assignmentNum] = value;
+            }
+        }
+
+        // indexer to get the first index of a given grade
+        public int this[char grade]
+        {
+            get
+            {
+                for(int i=0; i<grades.Length; i++)
+                {
+                    if (grades[i] == grade)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+        }
+
+        public IndexerDemo(int numAssignments)
+        {
+            grades = new char[numAssignments];
+        }
+
+        public void Display()
+        {
+            Console.WriteLine();
+            foreach (char grade in grades)
+            {
+                if (grade == (char)0)
+                {
+                    Console.Write("-");
+                }
+                else
+                {
+                    Console.Write(grade);
+                }
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Demos/Exam1Review/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Exam1Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            for(int n=0; n<20; n++)
+            {
+                Console.Write(Fib(n) + " ");
+            }
+            Console.WriteLine();
+
+            int x = 0;
+            while(true)
+            {
+                Console.WriteLine(x + ": " + Fib(x));
+                x++;
+            }
+            */
+            Fib(10);
+
+
+            IndexerDemo demo = new IndexerDemo(5);
+            demo[0] = 'A';
+            demo[2] = 'B';
+            demo[4] = 'C';
+            demo.Display();
+
+            Console.WriteLine(demo[0]);
+
+            Console.WriteLine(demo['F']);
+        }
+
+        // https://en.wikipedia.org/wiki/Fibonacci_sequence
+        static int Fib(int n)
+        {
+            // base cases - n = 0 or 1
+            if(n == 0)
+            {
+                Console.WriteLine(0);
+                return 0;
+            }
+            else if(n == 1)
+            {
+                Console.WriteLine(1);
+                return 1;
+            }
+
+            // recursive case
+            {
+                // state change == n goes down
+                // F(n) = F(n-1) + F(n-2)
+                int result = Fib(n - 1) + Fib(n - 2);
+                Console.WriteLine(result);
+                return result;
+            }
+        }
+    }
+}
