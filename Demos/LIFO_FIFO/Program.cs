190382d882d87bef43bb8ea4d239e76ec2e3f822
esmvcs <esmvcs@rit.edu>
2025-02-17 08:45:22 -0500

    LIFO & FIFO demo - M 2/17

---
+++ Demos/LIFO_FIFO/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace LIFO_FIFO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = new List<int>();
+            nums.Add(1);
+            nums.Add(6);
+            nums.Add(2);
+            nums.Add(9); // adds to the end
+            LIFO(nums);
+            FIFO(nums);
+        }
+
+        static void LIFO(List<int> nums)
+        {
+            // last in first out
+            for(int i = nums.Count-1; i>= 0; i-- )
+            {
+                Console.Write(nums[i]+" ");
+            }
+            Console.WriteLine();
+        }
+
+        static void FIFO(List<int> nums)
+        {
+            // first in first out
+            foreach(int n in nums)
+            {
+                Console.Write(n + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
