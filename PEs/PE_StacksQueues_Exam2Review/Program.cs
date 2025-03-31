c9c8f66b9f188ab2924e9a3962cbcbf77e3f1532
esmvcs <esmvcs@rit.edu>
2025-03-31 09:32:03 -0400

    stacks & queues notes in exam 2 review

---
+++ PEs/PE_StacksQueues_Exam2Review/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PE_StacksQueues_Exam2Review
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /** C == Count
+             *                  Stack          Queue
+             * Add to:          top (C-1)      back/end (C-1)
+             * Remove from:     top (C-1)      front (0)
+             */
+
+        }
+    }
+}
